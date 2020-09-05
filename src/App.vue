19ad2af831e051bd4bf5bce3444452726e5bfc56
eren <carlsagan120@gmail.com>
2020-09-06 02:08:04 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-      <div id='dropdownlist 'style="margin:50px auto 0; width:250px;">
+      <div id='dropdownlist' style="margin:50px auto 0; width:250px;">
         <br>
         <ejs-dropdownlist popupHeight="200px" popupWidth="250px" 
-        :dataSource='remoteData' :fields='remoteFields' placeholder='Select a name'
-        sortOrder='Descending'>
+          :dataSource='CarDropdownDataYear' :fields="fieldsYear"  placeholder='Years'
+          sortOrder='Descending' :select="onYearSelect">
+        </ejs-dropdownlist>
+        <ejs-dropdownlist popupHeight="200px" popupWidth="250px" 
+          :dataSource='CarDropdownDataMake' :fields="fieldsMake"  placeholder='Make'
+        sortOrder='Descending' :select="onMakeSelect">
+        </ejs-dropdownlist>
+        <ejs-dropdownlist popupHeight="200px" popupWidth="250px" 
+          :dataSource='CarDropdownDataModel' :fields="fieldsModel"  placeholder='Model'
+          sortOrder='Descending'>
         </ejs-dropdownlist>
       </div>
     </div>
@@ -14,26 +22,70 @@
 import Vue from 'vue';
 import { DropDownListPlugin } from "@syncfusion/ej2-vue-dropdowns";
 Vue.use(DropDownListPlugin);
-import { DataManager,WebApiAdaptor } from '@syncfusion/ej2-data';
-
-var remoteDataSource = new DataManager({
-  url: 'https://ej2services.syncfusion.com/production/web-services/api/Employees',
-  adaptor: new WebApiAdaptor,
-  crossDomain: true
-});
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
 
 export default Vue.extend({
   data: function() {
     return {
-      localData: [
-          { Id: 'game1', Game: 'Football' },
-          { Id: 'game2', Game: 'Basketball' },
-          { Id: 'game3', Game: 'Volleyball' }
-        ],
-      localField: { text: 'Game', value: 'Id' },
-      remoteData: remoteDataSource,
-      remoteFields: { text: 'FirstName', value: 'EmployeeID' }
+      CarDropdownDataYear: [],
+      CarDropdownDataModel: [],
+      CarDropdownDataMake: [],
+      selectedYear: null,
+      selectedMake: null,
+      fieldsYear: { text: 'year', value: 'id' },
+      fieldsMake: { text: 'make', value: 'id' },
+      fieldsModel: { text: 'model', value: 'id' }
     };
+  },
+  mounted() {
+    
+    axios.get('https://cors-anywhere.herokuapp.com/https://rateengine.ship.cars/v2/vehicles/years/?token=5cbe12fb62f4941267d623499a2a4fd5948fd3ef')
+          .then(response => { 
+            var data = response.data
+            for (var i = 0; i < data.length; i++) {
+              let tmp = {"year": 0, id: 0}
+              tmp.year = data[i].year
+              tmp.id = i 
+              this.CarDropdownDataYear.push(tmp)
+            }
+          })
+
+  },
+  computed: {
+  },
+  methods: {
+    onYearSelect: function(event) {
+      this.selectedYear = event.itemData.year
+
+      var url = 'https://cors-anywhere.herokuapp.com/https://rateengine.ship.cars/v2/vehicles/makes/?year=' + event.itemData.year + '&token=5cbe12fb62f4941267d623499a2a4fd5948fd3ef'
+      axios.get(url)
+        .then(response => { 
+          var data = response.data
+          for (var i = 0; i < data.length; i++) {
+            let tmp = {"make": "", id: 0}
+            tmp.make = data[i].make
+            tmp.id = i 
+            this.CarDropdownDataMake.push(tmp)
+          }
+        })
+    },
+    onMakeSelect: function(event) {
+      this.selectedMake = event.itemData.make
+      var url = 'https://cors-anywhere.herokuapp.com/https://rateengine.ship.cars/v2/vehicles/models/?year=' + this.selectedYear + '&make=' + this.selectedMake + '&token=5cbe12fb62f4941267d623499a2a4fd5948fd3ef'
+      axios.get(url)
+        .then(response => { 
+          var data = response.data
+          for (var i = 0; i < data.length; i++) {
+            let tmp = {"model": "", id: 0}
+            tmp.model = data[i].model
+            tmp.id = i 
+            this.CarDropdownDataModel.push(tmp)
+          }
+        })
+    }
   }
 });
 
